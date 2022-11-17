d5b870c596b41971c1275d1518d8f711c7546ce9
mvdbeek <m.vandenbeek@gmail.com>
2022-11-17 11:22:25 +0100

    Add epel

--- recipes/h5py/env.sh
+++ recipes/h5py/env.sh
@@ -1,2 +1,2 @@
-export CIBW_BEFORE_ALL_LINUX='yum install -y libhdf5-devel'
+export CIBW_BEFORE_ALL_LINUX='yum install –y epel-release && yum-config-manager --enable epel && yum install -y libhdf5-devel'
 export CIBW_SKIP='*-musllinux_* *-macosx_*'
